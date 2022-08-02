e3ee756c6b35b295519cccb04ccb87eef5b491c5
alexander papageorge <alexander.papageorge@openlabsusa.org>
2022-08-02 11:42:28 -0400

    it IS

--- py_monads/continuation_monad.py
+++ py_monads/continuation_monad.py
@@ -1,7 +1,23 @@
+from functools import reduce
+
+
 def unit(val):
     return lambda f: f(val)
 
 
+def bind(monad, kiesli):
+    return monad(kiesli)
+
+
+def associate(k0, k1):
+    return lambda x: k0(x)(k1)
+
+
+def run_monad(monad):
+    return monad(lambda x: x)
+
+
+######## another formulation ############
 def init(val):
     return unit(val)
 
@@ -16,3 +32,23 @@ end = lambda x: x
 if __name__ == '__main__':
     assert init(3)(add(7))(end) == 10
     assert init(20)(add(11))(add(35))(end) == 66
+    assert run_monad(init(3)(add(7))(add(4))) == 14
+
+    m = unit(10)
+    m = bind(m, lambda x: unit(x + 10))
+    m = bind(m, lambda x: unit(x + 33))
+    assert run_monad(m) == 53
+
+    m = unit(12)
+    k = associate(lambda x: unit(x+1), lambda x: unit(x+2))
+    m = bind(m, k)
+    assert run_monad(m) == 15
+
+    m = unit(23)
+    k = reduce(associate,
+               (lambda x: unit(x+3),
+                lambda x: unit(x*2),
+                lambda x: unit(x-2),
+                lambda x: unit(x/2)))
+    m = bind(m, k)
+    assert run_monad(m) == 25.0
